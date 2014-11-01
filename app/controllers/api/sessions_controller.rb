536ccb6c80bb7c4ae1638dfab080013255578d52
Petr Balaban <balaban.petr@gmail.com>
2014-11-01 20:44:56 +0400

    Session#create api method.

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApiController
+  respond_to(:json)
+
+  skip_before_action :verify_access_token
+
+  def create
+    @user = User.find_by(user_params.slice(:email))
+
+    #TODO: make it with devise method, handle inactive account, etc.
+    if @user.valid_password?(user_params[:password])
+      respond_with(@user, location: nil)
+    else
+      render json: { message: I18n.t(:"devise.failure.invalid", authentication_keys: :email) }, status: 422
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Wl::Application.routes.draw do
   namespace :api do
     scope :v1 do
       resources :users, only: :create
+      resources :sessions, only: :create
     end
   end
 end
