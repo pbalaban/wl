63a7af1d7f5f10944cbe915c0a12cbb0ab0bbf03
Petr Balaban <balaban.petr@gmail.com>
2014-11-01 20:08:19 +0400

    Fix seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@ u = User.new(
     email: 'admin@example.com',
     password: '1234',
     password_confirmation: '1234',
+    first_name: 'Super',
+    last_name: 'Admin',
     admin: true
 )
 u.skip_confirmation!
