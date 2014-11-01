e08882d5f3333655c1746d47d5e1ebe660bbebd7
Alexey Kisel <aki@jetthoughts.com>
2014-11-01 20:56:34 +0200

    Added link

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,10 @@ class Place < ActiveRecord::Base
   belongs_to :user
   has_many :exercises
   has_many :workouts
+
+
+  def current_workout
+    return @current_workout if @current_workout
+    @current_workout = workouts.where(date: Date.current).sorted.first
+  end
 end
